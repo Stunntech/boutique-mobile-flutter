932e3f69127112d68e6077d2fb6037f60a7edf03
“rosemarymannattu” <rosemary.stunntech@gmail.com>
2018-08-03 18:40:25 +0530

    design tweaks

--- lib/screens/cart_checkout_screens/shopping_cart.dart
+++ lib/screens/cart_checkout_screens/shopping_cart.dart
@@ -34,191 +34,191 @@ class MyShoppingCartPageState extends State<MyShoppingCartPage> {
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Container(
-              padding: new EdgeInsets.all(8.0),
-              decoration: new BoxDecoration(
+            _buildCustomAppBar(),
+            _buildCartList(),
+            _buildCartCheckoutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCartCheckoutButton() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (context) => new ShippingDetailPage()),
+            );
+          },
+          child: Container(
+            padding: new EdgeInsets.all(16.0),
+            decoration: new BoxDecoration(
+              color: const Color(0xFF33b17c),
+            ),
+            child: new Text(
+              'Proceed to checkout',
+              style: TextStyle(
                 color: Colors.white,
+                fontSize: 20.0,
+                fontFamily: 'avenir_medium',
               ),
-              child: new Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (context) => new SideNavigationPage()),
-                      );
-                    },
-                    child: new Container(
-                      margin: new EdgeInsets.only(top: 20.0),
-                      padding: new EdgeInsets.all(8.0),
-                      child: new Image.asset(
-                        "assets/images/menu_black.png",
-                      ),
-                    ),
-                  ),
-                  new Container(
-                    margin: new EdgeInsets.only(top: 25.0),
-                    child: new Text(
-                      'SHOPPING CART',
-                      textAlign: TextAlign.center,
-                      style: new TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontFamily: 'avenir_black',
-                        letterSpacing: 0.78,
-                      ),
-                    ),
-                  ),
-                  new GestureDetector(
-                    onTap: () {
-                      // Navigator.pop(context);
-                    },
-                    child: new Container(
-                      alignment: Alignment.topRight,
-                      margin: new EdgeInsets.only(top: 20.0),
-                      padding: new EdgeInsets.all(8.0),
-                      child: new Image.asset(
-                        "assets/images/shopping_bag_black.png",
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              textAlign: TextAlign.center,
             ),
+          ),
+        ),
+      ],
+    );
+  }
 
-            new Expanded(
-              child: new GridView.count(
-                childAspectRatio: 2.0,
-                crossAxisCount: 1,
-                mainAxisSpacing: 6.0,
-                children: _tiles.map((_MyShoppingCartTile item) {
-                  return new GridTile(
-                    child: new Container(
-                      margin: new EdgeInsets.only(left: 16.0, right: 16.0),
-                      child: new Column(
-                        children: <Widget>[
-                          new Expanded(
-                            child: new Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                new Expanded(
-                                  child: new Container(
-                                    height: 120.0,
-                                    width: 80.0,
-                                    child: new Image.asset(
-                                      item.imageUrl,
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                                new Expanded(
-                                  flex: 2,
-                                  child: new Container(
-                                    margin: new EdgeInsets.only(left: 20.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        new Container(
-                                          alignment: Alignment.topLeft,
-                                          child: new Text(
-                                            item.itemName,
-                                            textAlign: TextAlign.center,
-                                            style: new TextStyle(
-                                              color: const Color(0xFF989898),
-                                              fontSize: 21.0,
-                                              fontFamily: 'avenir_medium',
-                                            ),
-                                          ),
-                                        ),
-                                        new Container(
-                                          margin:
-                                              new EdgeInsets.only(top: 10.0),
-                                          alignment: Alignment.centerLeft,
-                                          child: new Text(
-                                            item.itemType,
-                                            textAlign: TextAlign.center,
-                                            style: new TextStyle(
-                                              color: const Color(0xFF989898),
-                                              fontSize: 14.0,
-                                              fontFamily: 'avenir_medium',
-                                            ),
-                                          ),
-                                        ),
-                                        new Container(
-                                          margin:
-                                              new EdgeInsets.only(top: 30.0),
-                                          alignment: Alignment.bottomLeft,
-                                          child: new Text(
-                                            item.itemCost,
-                                            textAlign: TextAlign.center,
-                                            style: new TextStyle(
-                                              color: const Color(0xFF989898),
-                                              fontSize: 14.0,
-                                              fontFamily: 'avenir_medium',
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          new Container(
-                            padding: new EdgeInsets.all(0.8),
-                            decoration: new BoxDecoration(
-                              color: const Color(0xFFe6e9ed),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
+  Widget _buildCartList() {
+    return new Expanded(
+      child: new GridView.count(
+        childAspectRatio: 2.5,
+        crossAxisCount: 1,
+        mainAxisSpacing: 16.0,
+        children: _tiles.map((_MyShoppingCartTile item) {
+          return new GridTile(
+            child: _buildCartListGridItem(item),
+          );
+        }).toList(),
+      ),
+    );
+  }
 
-            // checkout button
-
-            new Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
+  Widget _buildCartListGridItem(_MyShoppingCartTile item) {
+    return new Container(
+      margin: new EdgeInsets.only(left: 16.0, right: 16.0),
+      child: new Column(
+        children: <Widget>[
+          new Expanded(
+            child: new Row(
               children: <Widget>[
-                new GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) => new ShippingDetailPage()),
-                    );
-                  },
-                  child: Container(
-                    padding: new EdgeInsets.all(16.0),
-                    decoration: new BoxDecoration(
-                      color: const Color(0xFF33b17c),
+                new Expanded(
+                  child: new Container(
+                    height: 100.0,
+                    width: 50.0,
+                    child: new Image.asset(
+                      item.imageUrl,
+                      fit: BoxFit.contain,
                     ),
-                    child: new Text(
-                      'Proceed to checkout',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontFamily: 'avenir_medium',
-                      ),
-                      textAlign: TextAlign.center,
+                  ),
+                ),
+                new Expanded(
+                  flex: 2,
+                  child: new Container(
+                    margin: new EdgeInsets.only(left: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Container(
+                          child: new Text(
+                            item.itemName,
+                            style: new TextStyle(
+                              color: const Color(0xFF989898),
+                              fontSize: 21.0,
+                              fontFamily: 'avenir_medium',
+                            ),
+                          ),
+                        ),
+                        new Container(
+                          child: new Text(
+                            item.itemType,
+                            style: new TextStyle(
+                              color: const Color(0xFF989898),
+                              fontSize: 14.0,
+                              fontFamily: 'avenir_medium',
+                            ),
+                          ),
+                        ),
+                        new Container(
+                          margin: new EdgeInsets.only(top: 20.0),
+                          child: new Text(
+                            item.itemCost,
+                            style: new TextStyle(
+                              color: const Color(0xFF989898),
+                              fontSize: 14.0,
+                              fontFamily: 'avenir_medium',
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          new Container(
+            padding: new EdgeInsets.all(0.8),
+            decoration: new BoxDecoration(
+              color: const Color(0xFFe6e9ed),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCustomAppBar() {
+    return new Container(
+      padding: new EdgeInsets.all(8.0),
+      decoration: new BoxDecoration(
+        color: Colors.white,
+      ),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new SideNavigationPage()),
+              );
+            },
+            child: new Container(
+              margin: new EdgeInsets.only(top: 20.0),
+              padding: new EdgeInsets.all(8.0),
+              child: new Image.asset(
+                "assets/images/menu_black.png",
+              ),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(top: 25.0),
+            child: new Text(
+              'SHOPPING CART',
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                color: Colors.black,
+                fontSize: 14.0,
+                fontFamily: 'avenir_black',
+                letterSpacing: 0.78,
+              ),
+            ),
+          ),
+          new GestureDetector(
+            onTap: () {
+              // Navigator.pop(context);
+            },
+            child: new Container(
+              alignment: Alignment.topRight,
+              margin: new EdgeInsets.only(top: 20.0),
+              padding: new EdgeInsets.all(8.0),
+              child: new Image.asset(
+                "assets/images/shopping_bag_black.png",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/sidenavigation_and_tracking_screens/my_orders.dart
+++ lib/screens/sidenavigation_and_tracking_screens/my_orders.dart
@@ -26,167 +26,218 @@ class MyOrderPage extends StatefulWidget {
 }
 
 class MyOrderPageState extends State<MyOrderPage> {
+  double _TEXT_SIZE_ORDERS;
+  double _TEXT_SIZE_ITEM_NAME;
+  double _TEXT_SIZE_ITEM_COST;
+  double _TEXT_SIZE_ITEM_STATUS;
+  double _MARGIN_SMALL;
+  double _MARGIN_MEDIUM;
+  double _MARGIN_LARGE;
+  double _PADDING_SMALL;
+  double _LIST_ITEM_IMAGE_HEIGHT;
+  double _LIST_ITEM_IMAGE_WIDTH;
+
   @override
   Widget build(BuildContext context) {
+    var shortestSide = MediaQuery.of(context).size.shortestSide;
+    _setFontSizeAndStyle(shortestSide);
     return new Scaffold(
       body: new Container(
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Container(
-              padding: new EdgeInsets.all(8.0),
-              decoration: new BoxDecoration(
-                color: Colors.white,
-              ),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new GestureDetector(
-                    onTap: () {
-                      //  Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (context) => new SideNavigationPage()),
-                      );
-                    },
-                    child: new Container(
-                      margin: new EdgeInsets.only(top: 20.0),
-                      padding: new EdgeInsets.all(8.0),
-                      child: new Image.asset(
-                        "assets/images/menu_black.png",
-                      ),
-                    ),
-                  ),
-                  new Container(
-                    margin: new EdgeInsets.only(top: 25.0),
-                    child: new Text(
-                      'MY ORDERS',
-                      textAlign: TextAlign.center,
-                      style: new TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontFamily: 'avenir_black',
-                        letterSpacing: 0.78,
-                      ),
-                    ),
-                  ),
-                  new GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (context) => new MyShoppingCartPage()),
-                      );
-                    },
-                    child: new Container(
-                      alignment: Alignment.topRight,
-                      margin: new EdgeInsets.only(top: 20.0),
-                      padding: new EdgeInsets.all(8.0),
-                      child: new Image.asset(
-                        "assets/images/shopping_bag_black.png",
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            new Flexible(
-              child: new GridView.count(
-                childAspectRatio: 2.0,
-                crossAxisCount: 1,
-                mainAxisSpacing: 6.0,
-                children: _tiles.map((_MyOrderTile item) {
-                  return new GridTile(
-                    child: new Container(
-                      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
-                      child: new Card(
-                        child: new Row(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            new Expanded(
-                              child: new Container(
-                                height: 80.0,
-                                width: 80.0,
-                                child: new Image.asset(
-                                  item.imageUrl,
-                                  fit: BoxFit.scaleDown,
-                                ),
-                              ),
-                            ),
-                            new Expanded(
-                              child: new Container(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    new Container(
-                                      child: new Text(
-                                        item.itemName,
-                                        textAlign: TextAlign.center,
-                                        style: new TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16.0,
-                                          fontFamily: 'helvetica_neue_bold',
-                                        ),
-                                      ),
-                                    ),
-                                    new Container(
-                                      child: new Text(
-                                        item.itemCost,
-                                        textAlign: TextAlign.center,
-                                        style: new TextStyle(
-                                          color: const Color(0xFF33b17c),
-                                          fontSize: 18.0,
-                                          fontFamily: 'helvetica_neue_bold',
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            new Expanded(
-                                child: new Container(
-                              alignment: Alignment.center,
-                              decoration: new BoxDecoration(
-                                color: const Color(0xFFfafafa),
-                              ),
-                              // margin: new EdgeInsets.only(top: 25.0),
-                              child: new Text(
-                                item.itemDescription,
-                                textAlign: TextAlign.center,
-                                style: new TextStyle(
-                                  color: const Color(0xFF33b17c),
-                                  fontSize: 14.0,
-                                  fontFamily: 'helvetica_neue_bold',
-                                ),
-                              ),
-                            )),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
+            _buildCustomAppBar(),
+            _buildOrderList(),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildCustomAppBar() {
+    return new Container(
+      alignment: Alignment.center,
+      padding: new EdgeInsets.all(_PADDING_SMALL),
+      decoration: new BoxDecoration(
+        color: Colors.white,
+      ),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new GestureDetector(
+            onTap: () {
+              //  Navigator.pop(context);
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new SideNavigationPage()),
+              );
+            },
+            child: new Container(
+              margin: new EdgeInsets.only(top: _MARGIN_MEDIUM),
+              padding: new EdgeInsets.all(_PADDING_SMALL),
+              child: new Image.asset(
+                "assets/images/menu_black.png",
+              ),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(top: _MARGIN_LARGE),
+            child: new Text(
+              'MY ORDERS',
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                color: Colors.black,
+                fontSize: _TEXT_SIZE_ORDERS,
+                fontFamily: 'avenir_black',
+                letterSpacing: 0.78,
+              ),
+            ),
+          ),
+          new GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new MyShoppingCartPage()),
+              );
+            },
+            child: new Container(
+              alignment: Alignment.topRight,
+              margin: new EdgeInsets.only(top: _MARGIN_MEDIUM),
+              padding: new EdgeInsets.all(_PADDING_SMALL),
+              child: new Image.asset(
+                "assets/images/shopping_bag_black.png",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGridListItem(_MyOrderTile item) {
+    return new Container(
+      margin: new EdgeInsets.only(left: _MARGIN_SMALL, right: _MARGIN_SMALL),
+      child: new Card(
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              flex: 2,
+              child: new Container(
+                // alignment: Alignment.center,
+                height: _LIST_ITEM_IMAGE_HEIGHT,
+                width: _LIST_ITEM_IMAGE_WIDTH,
+                child: new Image.asset(
+                  item.imageUrl,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            new Expanded(
+              flex: 2,
+              child: new Container(
+                //margin: new EdgeInsets.only(left: 4.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new Container(
+                      child: new Text(
+                        item.itemName,
+                        style: new TextStyle(
+                          color: Colors.black,
+                          fontSize: _TEXT_SIZE_ITEM_NAME,
+                          fontFamily: 'helvetica_neue_bold',
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      child: new Text(
+                        item.itemCost,
+                        style: new TextStyle(
+                          color: const Color(0xFF33b17c),
+                          fontSize: _TEXT_SIZE_ITEM_COST,
+                          fontFamily: 'helvetica_neue_bold',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Expanded(
+                flex: 1,
+                child: new Container(
+                  alignment: Alignment.center,
+                  decoration: new BoxDecoration(
+                    color: const Color(0xFFfafafa),
+                  ),
+                  // margin: new EdgeInsets.only(top: 25.0),
+                  child: new Text(
+                    item.itemStatus,
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                      color: const Color(0xFF33b17c),
+                      fontSize: _TEXT_SIZE_ITEM_STATUS,
+                      fontFamily: 'helvetica_neue_bold',
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrderList() {
+    return new Flexible(
+      child: new GridView.count(
+        childAspectRatio: 2.5,
+        crossAxisCount: 1,
+        mainAxisSpacing: 6.0,
+        children: _tiles.map((_MyOrderTile item) {
+          return new GridTile(
+            child: _buildGridListItem(item),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  void _setFontSizeAndStyle(double shortestSide) {
+    if (shortestSide < 600) {
+      _LIST_ITEM_IMAGE_HEIGHT = 100.0;
+      _LIST_ITEM_IMAGE_WIDTH = 50.0;
+      _TEXT_SIZE_ORDERS = 12.0;
+      _TEXT_SIZE_ITEM_NAME = 14.0;
+      _TEXT_SIZE_ITEM_COST = 16.0;
+      _TEXT_SIZE_ITEM_STATUS = 12.0;
+      _MARGIN_SMALL = 10.0;
+      _MARGIN_MEDIUM = 20.0;
+      _MARGIN_LARGE = 25.0;
+      _PADDING_SMALL = 8.0;
+    } else if (shortestSide >= 600) {
+      _LIST_ITEM_IMAGE_HEIGHT = 140.0;
+      _LIST_ITEM_IMAGE_WIDTH = 70.0;
+      _TEXT_SIZE_ORDERS = 24.0;
+      _TEXT_SIZE_ITEM_NAME = 28.0;
+      _TEXT_SIZE_ITEM_COST = 32.0;
+      _TEXT_SIZE_ITEM_STATUS = 24.0;
+      _MARGIN_SMALL = 15.0;
+      _MARGIN_MEDIUM = 30.0;
+      _MARGIN_LARGE = 37.0;
+      _PADDING_SMALL = 12.0;
+    }
   }
 }
 
 class _MyOrderTile {
   final String imageUrl;
   final String itemName;
-  final String itemDescription;
+  final String itemStatus;
   final String itemCost;
 
-  _MyOrderTile(
-      this.imageUrl, this.itemName, this.itemDescription, this.itemCost);
+  _MyOrderTile(this.imageUrl, this.itemName, this.itemStatus, this.itemCost);
 }
